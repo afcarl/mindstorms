7265803377b6e2992b73ffcbb1260230f2a68126
Martin Vidner <martin@vidner.net>
2017-01-08 21:39:58 +0100

    Fixed bt-basics (undefined symbol)

--- bt-basics/mindstorms-ev3-linux-bt.py
+++ bt-basics/mindstorms-ev3-linux-bt.py
@@ -10,7 +10,7 @@ port = 1                   # the rfcomm port ev3 uses
 beep_command = \
     '\x0F\x00\x01\x00\x80\x00\x00\x94\x01\x81\x02\x82\xE8\x03\x82\xE8\x03'
 
-sock = bluetooth.BluetoothSocket(RFCOMM)
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
 sock.send(beep_command)
 sock.close()
